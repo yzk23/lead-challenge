7bc19bec5ef0eb2619e18fbb2fd1c295036ae113
Isaac Medeiros <isaac_koelho@hotmail.com>
2020-09-15 10:50:58 -0300

    alterando configurações do db

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/app.sqlite3'
 
 DB = SQLAlchemy(app)
 MIGRATE = Migrate(app, DB)
